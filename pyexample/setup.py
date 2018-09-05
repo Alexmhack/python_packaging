081c08afb1945052448d79f9977c525369f7c41e
alexHooper <alexmohack@gmail.com>
2018-09-05 17:23:20 +0530

    change the name of package

--- pyexample/setup.py
+++ pyexample/setup.py
@@ -4,10 +4,10 @@ with open('README.md') as fh:
 	long_description = fh.read()
 
 setuptools.setup(
-	name="pyexample",
+	name="pyexample_psg",
 	version="0.0.1",
-	author="PSG",
-	author_email="author@example.com",
+	author="psg",
+	author_email="pranavg456@gmail.com",
 	description="A small python example project",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
