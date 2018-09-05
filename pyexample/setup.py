907db74b1f435fcb4fa2f1efb4b6d9da92683197
alexHooper <alexmohack@gmail.com>
2018-09-05 15:22:01 +0530

    add url and classifier for package

--- pyexample/setup.py
+++ pyexample/setup.py
@@ -10,5 +10,12 @@ setuptools.setup(
 	author_email="author@example.com",
 	description="A small python example project",
 	long_description=long_description,
-	long_description_content_type="text/markdown"
+	long_description_content_type="text/markdown",
+	url="https://github.com/Alexmhack/python_packaging",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	]
 )
