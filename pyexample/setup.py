b98aa80e03452a1f7079b86eefb291171e140280
alexHooper <alexmohack@gmail.com>
2018-09-05 15:19:43 +0530

    add author and description of package

--- pyexample/setup.py
+++ pyexample/setup.py
@@ -6,4 +6,9 @@ with open('README.md') as fh:
 setuptools.setup(
 	name="pyexample",
 	version="0.0.1",
+	author="PSG",
+	author_email="author@example.com",
+	description="A small python example project",
+	long_description=long_description,
+	long_description_content_type="text/markdown"
 )
